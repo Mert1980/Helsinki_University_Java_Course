ba2366bf3321e4fe5150f407bed1fc5e7353f056
Mert <mertdemirok80@gmail.com>
2020-11-02 15:56:42 +0100

    add my solutions for exercises in part6

---
+++ PART06/part06-Part06_04.PrintingACollection/src/main/java/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Try out your class here
+        SimpleCollection j = new SimpleCollection("collection");
+        System.out.println(j);        
+        
+        System.out.println();
+        
+        j.add("element");
+        System.out.println(j);
+        
+        
+        
+        
+    }
+}
