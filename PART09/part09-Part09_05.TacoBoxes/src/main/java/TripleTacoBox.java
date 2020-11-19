b283e7223bf3e7f020b2d061b688441ca980a32a
Mert <mertdemirok80@gmail.com>
2020-11-19 06:35:48 +0100

    complete part09 exercise05 TacoBoxes

---
+++ PART09/part09-Part09_05.TacoBoxes/src/main/java/TripleTacoBox.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mertdemirok
+ */
+public class TripleTacoBox implements TacoBox{
+    
+    private int tacos;
+
+    public TripleTacoBox() {
+        this.tacos = 3;
+    }
+    
+    @Override
+    public int tacosRemaining() {
+        return tacos;
+    }
+
+    @Override
+    public void eat() {
+        tacos-=1;
+        if(tacos < 0){
+            tacos = 0;
+        }
+    }
+    
+}
