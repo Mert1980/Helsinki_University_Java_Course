66523556ea4d1542e4562b7cde2b44503fe1d602
Mert <mertdemirok80@gmail.com>
2020-11-22 18:02:08 +0100

    complete part10 exercise09 Reading Files Per Line

---
+++ PART10/part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -0,0 +1,36 @@
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class ReadingFilesPerLine {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+
+    public static List<String> read(String file) {
+        ArrayList<String> rows = new ArrayList<>();
+        try {
+            Files.lines(Paths.get(file)).forEach(row -> rows.add(row));
+            return rows;
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return rows;
+    }
+}
+
+//public static List<String> read(String file) {
+//        try {
+//            return Files.lines(Paths.get(file)).collect(Collectors.toList());
+//        } catch (Exception e) {
+//            System.out.println("Error: " + e.getMessage());
+//        }
+// 
+//        return new ArrayList<>();
+//    }
